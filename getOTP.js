20d578383a4a86d770a2b09745aad2b4d0b7f8a0
Abhay Mittal <abmittal@adobe.com>
2016-04-25 18:24:12 +0530

    getOTP => fixed path of 2fa file

--- getOTP.js
+++ getOTP.js
@@ -4,7 +4,7 @@ var SteamTotp = require('steam-totp');
 var responseObject;
 
 if(fs.existsSync("data\\***REMOVED***.2fa")) {
-	responseObject = JSON.parse(fs.readFileSync('***REMOVED***.2fa'));
+	responseObject = JSON.parse(fs.readFileSync('data/***REMOVED***.2fa'));
 }
 
 console.log(SteamTotp.getAuthCode(responseObject.shared_secret));
