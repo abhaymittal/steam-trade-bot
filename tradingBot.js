b09ea9220dd8bf8202072f75d019c7ed14d0cd39
Abhay Mittal <abmittal@adobe.com>
2016-05-01 13:33:24 +0530

    BOT => Updated log formatting

--- tradingBot.js
+++ tradingBot.js
@@ -293,20 +293,20 @@ setInterval(function(){backpacktf.heartbeat(config.steamid,config.bptfToken,logg
 // ------------------------------ Trade Events ------------------------------
 
 manager.on('newOffer', function(offer) {
-	logger.trade("Offer #" + offer.id + ": by " + offer.partner.getSteamID64());
+	logger.trade("Offer " + offer.id + ": by " + offer.partner.getSteamID64());
 	utilities.isUserBanned(offer.partner.getSteamID64(),logger,function (result) { //If user is banned, decline trade
 		if(result) { //User banned - decline trade
-			logger.info("User banned, declining");
+			logger.info("Offer "+offer.id+": User banned, declining");
 			offer.decline(function(err) {
 				if(err)
-					logger.error("Error occured while declining [user ban]");
+					logger.error("Offer "+offer.id+": Error occured while declining [user ban]");
 			});
 			return;
 		}
 		//Reject if trade will incur escrow hold
 		utilities.isEscrowHeld(offer,logger,function(result) {
 			if(result) {
-				logger.info("Declining trade due to escrow hold");
+				logger.info("Offer "+offer.id+": Declining trade due to escrow hold");
 				offer.decline();
 				return;
 			}
@@ -315,13 +315,13 @@ manager.on('newOffer', function(offer) {
 			//decline gift offers and offers which only take items
 			if((offer.itemsToGive==null)||(offer.itemsToReceive==null)) {
 				if(offer.itemsToGive==null)
-					logger.info("Empty selling list");
+					logger.info("Offer "+offer.id+": Empty selling list");
 				if(offer.itemsToReceive==null)
-					logger.info("Empty buying list");
-				logger.info("Declining trade as empty list");
+					logger.info("Offer "+offer.id+": Empty buying list");
+				logger.info("Offer "+offer.id+": Declining trade as empty list");
 				offer.decline(function(err) {
 					if(err)
-						logger.error("Error occured while declining offer "+offer.id);
+						logger.error("Offer "+offer.id+": Error occured while declining [empty list]");
 				});
 				return;
 			}
@@ -330,39 +330,39 @@ manager.on('newOffer', function(offer) {
 			
 			bp=utilities.buyingPrice(offer.itemsToReceive,buyDB,config.keyList,logger);
 			if(bp.metal==-1) {
-				logger.info("Buying list contains an invalid item, declining");
+				logger.info("Offer "+offer.id+": Invalid buy item, decline");
 				offer.decline();
 				return;
 			}
-			logger.info("The price of items to receive: "+bp.metal+ " metal and " +bp.keys+" Keys");
+			logger.info("Offer "+offer.id+": The price of items to receive: "+bp.metal+ " metal and " +bp.keys+" Keys");
 			sp=utilities.sellingPrice(offer.itemsToGive,sellDB,config.keyList,logger);
 			if(sp.metal==-1) {
-				logger.info("Selling list contains an invalid item, declining");
+				logger.info("Offer "+offer.id+": Invalid sell item, decline");
 				offer.decline();
 				return;
 			}
-			logger.info("The price of items to sell: "+sp.metal+ " metal and " +sp.keys+" Keys");
+			logger.info("Offer "+offer.id+": The price of items to sell: "+sp.metal+ " metal and " +sp.keys+" Keys");
 			//If bp >= sp, accept the trade
 			//if(((bp.keys>sp.keys)&&(bp.metal>=sp.metal))||((bp.keys==sp.keys)&&(Math.round(bp.metal*100)>=Math.round(sp.metal*100)))) {
 			if((bp.keys>=sp.keys)&&(Math.round(bp.metal*100)>=Math.round(sp.metal*100))) {
-				logger.info("Accepting offer");
+				logger.info("Offer "+offer.id+": Accept");
 				offer.accept(function(err) {
 					if(err)
 						logger.error("error occured while confirming");
 					else {
 						community.checkConfirmations();
-						logger.info("Offer accepted");
 						//decrease number of item in buy and sell list
 						utilities.decrementBuyStock(offer.itemsToReceive,buyDB,config.keyList);
 						utilities.decrementSellStock(offer.itemsToGive,sellDB,config.keyList);
 						utilities.saveBuyDB(buyDB);
 						utilities.saveSellDB(sellDB);
 						logger.info("DB Update => Updated both buy and sell DB");
+						logger.info("Offer "+offer.id+": Got "+bp.keys+" keys and "+bp.metal+" metal for "+sp.keys+" keys and "+sp.metal+" metal");
 					}
 				});
 			}
 			else {
-				logger.info("Declining as price offered is less");
+				logger.info("Offer "+offer.id+": Decline => Less price offered");
 				offer.decline();
 			}
 		});
@@ -370,7 +370,7 @@ manager.on('newOffer', function(offer) {
 });
 
 manager.on('receivedOfferChanged', function(offer, oldState) {
-	logger.trade("Offer #" + offer.id + ": " + TradeOfferManager.getStateName(oldState) + " -> " + TradeOfferManager.getStateName(offer.state));
+	logger.trade("Offer " + offer.id + ": " + TradeOfferManager.getStateName(oldState) + " -> " + TradeOfferManager.getStateName(offer.state));
 
 	if (offer.state == TradeOfferManager.ETradeOfferState.Accepted) {
 		offer.getReceivedItems(function(err, items) {
@@ -381,7 +381,7 @@ manager.on('receivedOfferChanged', function(offer, oldState) {
 					return item.name;
 				});
 
-				logger.trade("Offer #" + offer.id + ": Received: " + names.join(', '));
+				logger.trade("Offer " + offer.id + ": Received: " + names.join(', '));
 			}
 		});
 	}
